4b0090a5fe402f2c77e4a7776d25abcc7e2506cc
Mr.A <arafats144@gmail.com>
2026-09-09 21:54:49 +0300

    Updating the app

--- src/app/urban-warning/page.js
+++ src/app/urban-warning/page.js
@@ -248,11 +248,10 @@ export default function UrbanWarningPage() {
           [selectedArea.lat || 0.3476, selectedArea.lng || 32.5825],
           12
         );
-        L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
           attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
           maxZoom: 19,
-          subdomains: "abcd",
         }).addTo(map);
         mapInstanceRef.current = map;
         markersLayerRef.current = L.layerGroup().addTo(map);
